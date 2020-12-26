2a4948b32cf6990239cffdb37321417ae7940cbe
sego-w <64610274+sego-w@users.noreply.github.com>
2020-12-26 15:51:04 +0200

    Add files via upload
    IT FINALLY WORKS! :DDD

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,9 @@
 ### IMPORTANT! SINCE I AM LAZY YOU WILL NOT WIN/LOSE IN THE CASE OF A DIAGONAL ###
 from time import sleep
 from random import randint
+import sys
+from sys import exit
+import os
 def display(a,b,c):
     print(','.join(a))
     print(','.join(b))
@@ -8,25 +11,39 @@ def display(a,b,c):
 # Tic Tac Toe
 # December 2020
 # Initial Playfield
-row1 = [' ',' ',' ']
-row2 = [' ',' ',' ']
-row3 = [' ',' ',' ']
+def initial_state():
+    global gamewin
+    global gamelose
+    global row1
+    global row2
+    global row3
+    global user_sym
+    global opposite
+    row1 = [' ',' ',' ']
+    row2 = [' ',' ',' ']
+    row3 = [' ',' ',' ']
+    repeat_game = 'Irrelevant string'
+    gamewin = False
+    gamelose = False
 # Starting the game!
-print("Welcome to Tic Tac Toe! Here is your board: ")
-display(row1,row2,row3)
-user_sym = input("Would you like to be X or O? ").upper()
-if user_sym == 'O':
+    print("Welcome to Tic Tac Toe! Here is your board: ")
+    display(row1,row2,row3)
+    user_sym = input("Would you like to be X or O? ").upper()
+    if user_sym == 'O':
         opposite = 'X'
-else:
+    else:
         opposite = 'O'
+initial_state()
 # Choosing the row and column
 def __main__():
     global pick_row
     global pick_pos
     pick_row = "Not a digit"
-    while pick_row.isdigit() == False:
+    pick_pos = "Also not a digit"
+    while isinstance(pick_row,int) == False:
         pick_row = int(input("Pick your row! "))
-    pick_pos = int(input(f"Which place would you like to place your {user_sym}? "))
+    while isinstance(pick_pos,int) == False: 
+        pick_pos = int(input(f"Which place would you like to place your {user_sym}? "))
     # Editing the row/column
     if pick_row == 1:
         if pick_pos == 1:
@@ -55,6 +72,25 @@ def __main__():
     display(row1,row2,row3)
 __main__()
 
+#Check if the game is over
+def gameovercheck():
+    global gamewin
+    global gamelose
+    if row1[0] == row1[1] == row1[2] == user_sym or row2[0] == row2[1] == row2[2] == user_sym or row3[0] == row3[1] == row3[2] == user_sym:
+        gamewin = True
+    elif row1[0] == row1[1] == row1[2] == opposite or row2[0] == row2[1] == row2[2] == opposite or row3[0] == row3[1] == row3[2] == opposite:
+        gamelose = True
+    elif row1[0] == row2[0] == row3[0] == user_sym or row1[1] == row2[1] == row3[1] == user_sym or row1[2] == row2[2] == row3[2] == user_sym:
+        gamewin = True
+    elif row1[0] == row2[0] == row3[0] == opposite or row1[1] == row2[1] == row3[1] == opposite or row1[2] == row2[2] == row3[2] == opposite:
+        gamelose = True
+    if gamewin == True:
+        print("Congrats! You won!")
+    if gamelose == True:
+        print("You lost lulz")
+
+gameovercheck()
+
 # Placing an X or O randomly
 
 def robot_hax():
@@ -64,17 +100,29 @@ def robot_hax():
     bot_row = randint(1,3)
     bot_pos = randint(1,3)
     if bot_row == 1:
+        while row1[bot_pos-1] != ' ':
+            bot_pos = randint(1,3)
+    elif bot_row == 2:
+        while row2[bot_pos-1] != ' ':
+            bot_pos = randint(1,3)
+    elif bot_row == 3:
+        while row3[bot_pos-1] != ' ':
+            bot_pos = randint(1,3)
+    # Row 1
+    if bot_row == 1:
         if row1[bot_pos-1] == ' ':
             if user_sym == 'O':
               row1[bot_pos-1] = 'X'
             elif user_sym == 'X':
               row1[bot_pos-1] = 'O'
+    # Row 2
     if bot_row == 2:
         if row2[bot_pos-1] == ' ':
             if user_sym == 'O':
               row2[bot_pos-1] = 'X'
             elif user_sym == 'X':
               row2[bot_pos-1] = 'O'
+    # Row 3
     if bot_row == 3:
         if row3[bot_pos-1] == ' ':
             if user_sym == 'O':
@@ -82,42 +130,30 @@ def robot_hax():
             elif user_sym == 'X':
               row3[bot_pos-1] = 'O'
     display(row1,row2,row3)
-robot_hax() 
+    
 
-sleep(1)
-print("Your turn!")
-sleep(1)
-#Check if the game is over
-gamewin = False
-gamelose = False
-def gameovercheck():
-    global gamewin
-    global gamelose
-    if row1[0] == row1[1] == row1[2] or row2[0] == row2[1] == row2[2] or row3[0] == row3[1] == row3[2] == user_sym:
-        gamewin = True
-    elif row1[0] == row1[1] == row1[2] == opposite or row2[0] == row2[1] == row2[2] == opposite or row3[0] == row3[1] == row3[2] == opposite:
-        gamelose = True
-    elif row1[0] == row2[0] == row3[0] == user_sym or row1[1] == row2[1] == row3[1] == user_sym or row1[2] == row2[2] == row3[2] == user_sym:
-        gamewin = True
-    elif row1[0] == row2[0] == row3[0] == opposite or row1[1] == row2[1] == row3[1] == opposite or row1[2] == row2[2] == row3[2] == opposite:
-        gamelose = True
-gameovercheck()
+if gamewin == False and gamelose == False:
+    robot_hax() 
+    gameovercheck()
 
-while gamewin != True or gamelose != True:
-    for i in range(1,9):
+def whole_program():
+    while gamewin == False or gamelose == False:
         __main__()
+        gameovercheck()
         robot_hax()
         gameovercheck()
+        if gamewin == True or gamelose == True:
+            restart_check = input("Would you like to play again? (Y/N) ").upper()
+            if restart_check == 'Y':
+                initial_state()
+                whole_program()
+            if restart_check == 'N':
+                exit("Okay then! Goodbye...")
         sleep(1)
         print("Your turn!")
         sleep(1)
+    
+whole_program()
 
-# Game over!
-if gamewin:
-    print("Congrats! You won!")
-    exit
-if gamelose:
-    print("Wow u lost lulz")
-    exit
-# Idiot user use case
+
 
